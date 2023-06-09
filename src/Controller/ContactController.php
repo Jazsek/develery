59724e5e31bfe4ad48bb64515c1293cf4e5d18d3
Jazsek <jaszek94@gmail.com>
2023-06-09 10:32:37 +0200

    Form class használata

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,10 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Form\ContactType;
 use App\Repository\ContactRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -15,14 +15,7 @@ class ContactController extends AbstractController
     #[Route('/', name: 'app_contact')]
     public function index(Request $request, ContactRepository $contacts): Response
     {
-        $contact = new Contact();
-        $form = $this->createFormBuilder($contact)
-            ->add('name')
-            ->add('email')
-            ->add('message')
-            ->add('submit', SubmitType::class, ['label' => 'Mentés'])
-            ->getForm();
-
+        $form = $this->createForm(ContactType::class, new Contact());
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
@@ -40,9 +33,5 @@ class ContactController extends AbstractController
                 'form' => $form
             ]
         );
-
-        /* return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]); */
     }
 }
